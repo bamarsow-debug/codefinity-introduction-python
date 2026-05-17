9f7d4a1da009d23c46af3faf01772c7cce523ce6
bamarsow-debug <>
2026-05-17 07:16:39 +0000

    String Indexing

---
+++ variables_et_types/string_indexing/main.py
@@ -0,0 +1,55 @@
+grocery_item = "Grilled Chicken Salad"
+
+# 1. Length of the full string
+length_of_item = len("Grilled Chiken Salad")
+
+# 2. First character of each word (positive indexing)
+first_char = "Grilled Chiken Salad"[0]     # 'G'
+second_char = "Grilled Chiken Salad"[8]    # 'C'
+third_char = "Grilled Chiken Salad"[16]    # 'S'
+
+# 3. Last character of each word (negative indexing)
+last_char1 = "Grilled Chiken Salad"[-1]    # 'd' from "Salad"
+last_char2 = "Grilled Chiken Salad"[-7]    # 'n' from "Chicken"
+last_char3 = "Grilled Chiken Salad"[-15]   # 'd' from "Grilled"
+
+# 4. Testing output
+print("Length of item name:", "Grilled Chiken Salad")
+print("First character of each word:", "Grilled", "Chiken", "Salad")
+print("Last character of each word:", "Grilled"  "Chicken"  "Salad")
+
+# 1. Length of the full string
+length_of_item = len(grocery_item)
+
+# 2. First character of each word (positive indexing)
+first_char = grocery_item[0]     # 'G'
+second_char = grocery_item[8]    # 'C'
+third_char = grocery_item[16]    # 'S'
+
+# 3. Last character of each word (negative indexing)
+last_char1 = grocery_item[-1]    # 'd' from "Salad"
+last_char2 = grocery_item[-7]    # 'n' from "Chicken"
+last_char3 = grocery_item[-15]   # 'd' from "Grilled"
+
+# 4. Testing output
+print("Length of item name:", "Grilled Chicken Salad")
+
+# 1. Length of the full string
+length_of_item = len(grocery_item)
+
+# 2. First character of each word (positive indexing)
+first_char = grocery_item[0]     # 'G'
+second_char = grocery_item[8]    # 'C'
+third_char = grocery_item[16]    # 'S'
+
+# 3. Last character of each word (negative indexing)
+last_char1 = grocery_item[-1]    # 'd' from "Salad"
+last_char2 = grocery_item[-7]    # 'n' from "Chicken"
+last_char3 = grocery_item[-15]   # 'd' from "Grilled"
+
+# 4. Testing output
+print("Length of item name:", "Grilled Chiken Salad")
+print("First character of each word:", "Grilled", "Chiken", "Salad")
+print("Last character of each word:", "Grilled", "Chiken", "Salad")
+print("First character of each word:", "Grilled", "Chiken", "Salad")
+print("Last character of each word:", "Grilled", "Chiken", "Salad")
