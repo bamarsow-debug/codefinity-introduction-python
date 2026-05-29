beace3883651377f6ed70cff5a0a06f08f0aa1de
bamarsow-debug <>
2026-05-29 06:01:01 +0000

    Range function

---
+++ boucles/range_function/main.py
@@ -0,0 +1,10 @@
+# List of products on promotion for each weekday
+daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
+for i in range(len(daily_promotions)):
+     print(weekdays[i])
+for weekday in weekdays:
+     print(weekda
+    
+     print(weekday, "promotion on:", daily_promotions)
+    
+
